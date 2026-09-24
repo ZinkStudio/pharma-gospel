5a3b57e553a562586474c3d572c294b4ef92c491
ZinkStudio <zink13013@gmail.com>
2026-09-24 20:59:26 +0200

    @ordoscan : comment local, uncomment cdn

--- utils/ordoscan-bgremoval.js
+++ utils/ordoscan-bgremoval.js
@@ -1,10 +1,10 @@
 // /static/js/assistants/assistant-ordoscan-bgremoval.js
 
 const IMGLY_VERSION = '1.5.8';
-// const IMGLY_MODULE_URL = `https://esm.sh/@imgly/background-removal@${IMGLY_VERSION}?bundle`;
-// const IMGLY_PUBLIC_PATH = `https://staticimgly.com/@imgly/background-removal-data/${IMGLY_VERSION}/dist/`;
-const IMGLY_MODULE_URL = new URL(`../vendor/imgly@1.5.8/background-removal.mjs`, import.meta.url).href; // Chargement local pour éviter les problèmes de CORS et de CDN
-const IMGLY_PUBLIC_PATH = new URL(`../vendor/imgly@1.5.8/dist/`, import.meta.url).href;
+const IMGLY_MODULE_URL = `https://esm.sh/@imgly/background-removal@${IMGLY_VERSION}?bundle`;
+const IMGLY_PUBLIC_PATH = `https://staticimgly.com/@imgly/background-removal-data/${IMGLY_VERSION}/dist/`;
+// const IMGLY_MODULE_URL = new URL(`../vendor/imgly@1.5.8/background-removal.mjs`, import.meta.url).href; // Chargement local pour éviter les problèmes de CORS et de CDN
+// const IMGLY_PUBLIC_PATH = new URL(`../vendor/imgly@1.5.8/dist/`, import.meta.url).href;
 
 let _imglyModulePromise = null;
 function loadImgly() {
